7c52f6d1f64ff981234e3e1f2dcd7b6cfe559e56
svadhis <imeian@gmail.com>
2022-05-20 16:39:39 +0200

    correction

--- js/card.test.js
+++ js/card.test.js
@@ -1,3 +1,23 @@
 const Card = require('./card');
 
-// Your tests here
+let card;
+
+beforeEach(() => {
+    card = new Card('');
+  });
+
+test('when we create a card, it should not be visible', () => {
+    expect(card.state).toBe(0);
+});
+
+test('when we turn a card that was not visible, it should be visible', () => {
+    card.state = 0;
+    card.turn();
+    expect(card.state).toBe(1);
+});
+
+test('when we turn a card that was visible, it should not be visible', () => {
+    card.state = 1;
+    card.turn();
+    expect(card.state).toBe(0);
+});
--- php/tests/AnimalTest.php
+++ php/tests/AnimalTest.php
@@ -9,6 +9,23 @@ final class AnimalTest extends TestCase
 {
     public function testNoMoreHungryWhenEats(): void
     {
-       // Your test here
+        $animal = new Tiger([
+            'name' => 'Tigrou',
+            'age' => 4,
+            'size' => 120,
+            'weight' => 450,
+            'enclos_id' => 0,
+        ]);
+
+        $animal->isHungry = 1;
+
+        $animal->setDatabase($this->createStub(Database::class));
+
+        $animal->eat();
+
+        $this->assertEquals(
+            0,
+            $animal->isHungry
+        );
     }
 }
